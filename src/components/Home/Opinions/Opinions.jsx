30f1e418995d798ebc47b8ef2f3035dfb5a2197d
rocktohq <itzmonir@gmail.com>
2023-11-25 04:15:29 +0600

    Opinions Added

---
+++ src/components/Home/Opinions/Opinions.jsx
@@ -0,0 +1,12 @@
+import Container from "../../shared/Container";
+import Title from "../../shared/Title";
+
+const Opinions = () => {
+  return (
+    <Container className="py-20 bg-cyan-50">
+      <Title heading="Oninions" subHeading="What users say" big center />
+    </Container>
+  );
+};
+
+export default Opinions;

b1fe81e9ed065ea15554350f792624520a25f076
rocktohq <itzmonir@gmail.com>
2023-11-25 07:02:31 +0600

    Loader Added

---
+++ src/components/shared/Loader.jsx
@@ -0,0 +1,11 @@
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const Loader = () => {
+  return (
+    <div className="my-12 flex justify-center items-center">
+      <PacmanLoader color="#36d7b7" />
+    </div>
+  );
+};
+
+export default Loader;

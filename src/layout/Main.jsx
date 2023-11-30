1337abfc95860a852198419bf7b661fc861cde27
rocktohq <itzmonir@gmail.com>
2023-11-30 09:54:57 +0600

    Main Layout Updated

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,18 +1,13 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header/Header";
 import Footer from "../components/shared/Footer/Footer";
 
 const Main = () => {
-  const location = useLocation();
-  const noFooter =
-    location.pathname.includes("articles") ||
-    location.pathname.includes("premium-articles");
-
   return (
     <>
       <Header />
       <Outlet />
-      {noFooter || <Footer />}
+      <Footer />
     </>
   );
 };
